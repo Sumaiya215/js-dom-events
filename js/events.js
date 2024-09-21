a51522c7039a1c408239cec3a38c9666ae9320b0
Sumaiya215 <sumaiyacse15>
2024-09-21 10:38:32 -0700

    added js dom events

---
+++ js/events.js
@@ -0,0 +1,39 @@
+// option-1:directly set on the HTML element
+
+// option-2: add onclick function !Important
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+// option-3: 
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        }
+
+// option-3: another
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple';
+}
+
+// option-4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click',makePink);
+
+function makePink(){
+     document.body.style.backgroundColor = 'pink';
+}
+
+// option-4: another
+const greenButton = document.getElementById('make-green');
+greenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+})
+
+// option-4:final !Important
+document.getElementById('make-bisque').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'bisque';
+  })
